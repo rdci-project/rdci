39eba2f1886704bdbece637b29279f80579d84c7
Steinar Sturlaugsson <costeinar@gmail.com>
2018-03-30 13:14:39 +0200

    reb

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p ./tmp
+cd ./tmp
+wget -nc https://dist.ipfs.io/go-ipfs/v0.4.14/go-ipfs_v0.4.14_darwin-amd64.tar.gz 
+tar -xzvf go-ipfs_v0.4.14_darwin-amd64.tar.gz
+cd go-ipfs
+./install.sh
+
