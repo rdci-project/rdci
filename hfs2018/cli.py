3a90a754268e7a9add4c0441dde10eaf68bebb7e
Johan Dahlberg <johan@uppsala-bioinformatics.se>
2018-04-13 21:52:47 +0200

    print -> click.echo

--- hfs2018/cli.py
+++ hfs2018/cli.py
@@ -16,33 +16,33 @@ def main():
 @main.command("init")
 def init():
     if not os.path.exists(IPFS_BIN_LOCATION):
-        print("You don't have IPFS. Don't worry I'll download it for you...")
+        click.echo("You don't have IPFS. Don't worry I'll download it for you...")
         download_ipfs()
 
     if os.path.exists(os.path.join(os.environ.get("HOME"), ".ipfs")):
-        print("IPFS has already been inititated. Skipping that...")
+        click.echo("IPFS has already been inititated. Skipping that...")
     else:
-        print("Initiating IPFS")
+        click.echo("Initiating IPFS")
         exit_code = subprocess.call([IPFS_BIN_LOCATION, "init"])
         if exit_code == 0:
-            print("Successfully initiated IPFS")
+            click.echo("Successfully initiated IPFS")
         else:
-            print("Failed to initiate IPFS")
+            click.echo("Failed to initiate IPFS")
             sys.exit(1)
 
 
 
 @main.command("add")
 def add():
-    print("Add")
+    click.echo("Add")
 
 @main.command("run")
 def run():
-    print("Run")
+    click.echo("Run")
 
 @main.command("publish")
 def publish():
-    print("Publish")
+    click.echo("Publish")
 
 
 if __name__ == '__main__':
